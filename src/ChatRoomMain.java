6e663dc3ae0726ff0106d572e9be5eeb937448d9
Andrew Struthers <struthers919@gmail.com>
2018-01-09 16:15:40 -0800

    Base client and host added

--- src/ChatRoomMain.java
+++ src/ChatRoomMain.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ChatRoomMain {
 	/**
@@ -5,7 +11,32 @@ public class ChatRoomMain {
 	 * Andrew G Struthers
 	 * 
 	 */
-	public static void main(String[] args){
-		
+	static String ip;
+	static Scanner input = new Scanner(System.in);
+	
+	public static void client() throws UnknownHostException, IOException{
+		System.out.print("Enter host ip address: ");
+		String host_ip = input.nextLine();
+		Socket s = new Socket(host_ip ,12345);
+		System.out.println(s.getLocalAddress());
+		s.close();
+	}
+	
+	public static void host() throws IOException{
+		ServerSocket s = new ServerSocket(12345);
+		Socket conn = s.accept();
+		System.out.println(conn.getInetAddress());
+		conn.close();
+		s.close();
+	}
+	
+	public static void main(String[] args) throws UnknownHostException{
+		ip = InetAddress.getLocalHost().getHostAddress();
+		System.out.println(ip);
+		/*
+		 * TODO we need to either call client() to connect, or host() to open
+		 * we need to call host() first, theoretically it should run on two different computers
+		 * 
+		 */
 	}
 }
